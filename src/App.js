f5975966fcc254a53e8aac90daf792e3a766a695
EDMOND G <gihozoedmond@gmail.com>
2021-04-27 17:36:33 +0200

    workin on new look

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useState } from "react";
-import Particles from "react-particles-js";
-import particlesConfig from "./config/particlesConfig";
+// import { useState } from "react";
+// import Particles from "react-particles-js";
+// import particlesConfig from "./config/particlesConfig";
 import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
@@ -12,19 +12,16 @@ import Navbar from "./layouts/NavBar";
 import Footer from "./layouts/Footer";
 
 function App() {
-	const [cursorX, setCursorX] = useState(null);
-	const [cursorY, setCursorY] = useState(null);
+	// const [cursorX, setCursorX] = useState(null);
+	// const [cursorY, setCursorY] = useState(null);
 
-	window.addEventListener("mousemove", (e) => {
-		setCursorX(e.pageX);
-		setCursorY(e.pageY);
-	});
+	// window.addEventListener("mousemove", (e) => {
+	// 	setCursorX(e.pageX);
+	// 	setCursorY(e.pageY);
+	// });
 
 	return (
 		<BrowserRouter>
-			<div style={{ position: "absolute" }}>
-				<Particles height="100vh" width="100vw" params={particlesConfig} />
-			</div>
 			<Navbar />
 			<Switch>
 				<Route component={Home} path="/" exact />
@@ -33,7 +30,7 @@ function App() {
 				<Route component={Post} path="/post/:slug" />
 				<Route component={Portfolio} path="/portfolio" />
 			</Switch>
-			<div className="cursor" style={{ left: cursorX, top: cursorY }}></div>
+			{/* <div className="cursor" style={{ left: cursorX, top: cursorY }}></div> */}
 			<Footer />
 		</BrowserRouter>
 	);
