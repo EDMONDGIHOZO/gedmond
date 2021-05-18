efde02048de0eea8c90dfb6e58be697bb2709ae1
EDMOND G <gihozoedmond@gmail.com>
2021-05-18 17:35:07 +0200

    add emojis"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,93 @@
-import React, {useRef} from "react";
-import { motion } from 'framer-motion';
-
-
-
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { FaBed } from "react-icons/fa";
+import { IoFastFood } from "react-icons/io5";
+import { IoCodeSlashSharp } from "react-icons/io5";
 export default function Home() {
+	const constraintsRef = useRef(null);
 
-    const constraintsRef = useRef(null);
+	const buttonVar = {
+		hover: {
+			scale: 1.1,
+			textShadow: "0px 0px 8px rgb(255, 255, 255)",
+			fontWeight: "bold",
+		},
+	};
 
-  return (
-      <div className="flex flex-wrap content-center bg-gray-400 dark:bg-gray-800 w-full h-screen text-center md:flex-row">
-              <motion.div
-                  className="flex items-center justify-center bg-gray-200 w-full md:w-1/2 leftSide column">
-                  <div className="max-w-lg transition-all ease-in-out duration-1000 transform translate-x-0 slide">
-                      <div className="example-container">
-                          <motion.div className="drag-area" ref={constraintsRef} />
-                          <motion.div drag dragConstraints={constraintsRef}>
-                              <motion.h1 animate={{ marginTop: 1, opacity: 1}} initial={{ marginTop:-400, opacity:0.2}}
-                                         transition={{ type: "spring", delay:0.3, stiffness: 50, restSpeed: 0.5 }}
-                                         className="text-2xl mb-4 transition duration-500 ease-in-out hover:text-gray-800 font-bold textShadow tracking-wide text-white dark:text-white md:text-7xl">
-                                  EDMOND GIHOZO
-                              </motion.h1>
-                          </motion.div>
-                      </div>
-                      <p className="mt-2 text-gray-600 mx-auto md:my-6">
-                          Javascript Developer - Full-Stack 💥✔
-                      </p>
-                  </div>
-              </motion.div>
+	return (
+		<div className="flex flex-wrap content-center bg-gray-400 dark:bg-gray-800 w-full h-screen text-center md:flex-row">
+			<motion.div className="flex items-center justify-center bg-gray-200 w-full md:w-1/2 leftSide column">
+				<div className="max-w-lg transition-all ease-in-out duration-1000 transform translate-x-0 slide">
+					<div className="example-container">
+						<motion.div className="drag-area" ref={constraintsRef} />
+						<motion.div drag dragConstraints={constraintsRef}>
+							<motion.h1
+								animate={{ marginTop: 1, opacity: 1 }}
+								initial={{ marginTop: -400, opacity: 0.2 }}
+								transition={{
+									type: "spring",
+									delay: 0.3,
+									stiffness: 50,
+									restSpeed: 0.5,
+								}}
+								className="text-2xl mb-4 transition duration-500 ease-in-out hover:text-gray-800 font-bold textShadow tracking-wide text-white dark:text-white md:text-7xl"
+							>
+								EDMOND GIHOZO
+							</motion.h1>
+						</motion.div>
+					</div>
+					<p className="mt-2 text-gray-600 mx-auto md:my-6">
+						Fullstack Developer & Designer 💥✔
+					</p>
+				</div>
+			</motion.div>
 
-              <motion.div
-                      className="flex items-center justify-center w-full md:w-1/2 rightSide column hidden md:flex">
-                      <motion.div animate={{opacity:0.1, scale:1 , type:'spring', x:200}} initial={{scale:2 , x:200 }} transition={{ type: 'spring', restSpeed: 0.1 }} className='box1'></motion.div>
-                      <motion.div animate={{opacity:0.3, scale:1 , type:'tween', x:40}} initial={{scale:2 , x:200 }} transition={{ type: 'tween', restSpeed: 0.2 }} className='box2'></motion.div>
-                      <motion.div animate={{opacity:0.2, scale:1 , type:'spring', x:100}} initial={{scale:2 , x:200 }} transition={{ type: 'spring', restSpeed: 0.3 }} className='box3'></motion.div>
-                      <motion.h2 className='md:text-4xl font-light text-left text-white titleText'
-                                 initial={{fontSize:'80px'}}
-                                 transition={{ type: 'spring', stiffness: 50, restSpeed: 0.5 }}
-                                 animate={{
-                                     color: '#ffffff', fontSize: '36px', type:'spring'
-                                 }}>
-                          PORTFOLIO
-                      </motion.h2>
-              </motion.div>
-      </div>
-  );
+			<motion.div className="items-center justify-center w-full md:w-1/2 rightSide column hidden md:flex">
+				<motion.div
+					animate={{ opacity: 0.1, scale: 1, type: "spring", x: 200 }}
+					initial={{ scale: 2, x: 200 }}
+					transition={{ type: "spring", restSpeed: 0.1 }}
+					className="box1 hover:bg-red-500 box"
+					whileHover={{ scale: 2 }}
+				>
+					<FaBed size="4rem" />
+				</motion.div>
+				<motion.div
+					animate={{ opacity: 0.3, scale: 1, type: "tween", x: 40 }}
+					initial={{ scale: 2, x: 200 }}
+					transition={{ type: "tween", restSpeed: 0.2 }}
+					className="box2 hover:bg-red-600 box"
+					whileHover={{ scale: 2 }}
+				>
+					<IoFastFood size="4rem" />
+				</motion.div>
+				<motion.div
+					animate={{ opacity: 0.2, scale: 1, type: "spring", x: 100 }}
+					initial={{ scale: 2, x: 200 }}
+					transition={{ type: "spring", restSpeed: 0.3 }}
+					className="box3 hover:bg-red-700 box"
+					whileHover={{ scale: 2 }}
+				>
+					<IoCodeSlashSharp size="4rem" />
+				</motion.div>
+				<Link to={"/"}>
+					<motion.h2
+						className="md:text-4xl p-3 font-light text-left text-white titleText"
+						initial={{ fontSize: "80px", backgroundColor: "transparent" }}
+						variants={buttonVar}
+						transition={{ type: "spring", stiffness: 50, restSpeed: 0.5 }}
+						whileHover="hover"
+						animate={{
+							color: "#ffffff",
+							fontSize: "36px",
+							type: "spring",
+						}}
+					>
+						PORTFOLIO
+					</motion.h2>
+				</Link>
+			</motion.div>
+		</div>
+	);
 }
