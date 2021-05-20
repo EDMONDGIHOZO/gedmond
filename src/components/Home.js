c592da3215e6be43f7536a37d8e7215514f14a1e
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-05-20 17:07:44 +0200

    work on the profile page View

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,126 +6,121 @@ import { IoFastFood } from "react-icons/io5";
 import { IoCodeSlashSharp } from "react-icons/io5";
 
 const buttonVar = {
-	hover: {
-		scale: 1.1,
-		textShadow: "0px 0px 8px rgb(255, 255, 255)",
-		fontWeight: "bold",
-	},
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255, 255, 255)",
+    fontWeight: "bold",
+  },
 };
 
 const containerVarients = {
-	hidden: {
-		opacity: 0,
-	},
-	visible: {
-		opacity: 0,
-		transition: {
-			delay: 0.8,
-			duration: 0.8,
-		},
-	},
-	exit: {
-		x: "-100vw",
-		transition: { ease: "easeInOut" },
-	},
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 0,
+    transition: {
+      delay: 0.8,
+      duration: 0.8,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
 };
 
-
-
 const Home = () => {
-	const constraintsRef = useRef(null);
+  const constraintsRef = useRef(null);
 
+  return (
+    <motion.div
+      className="flex flex-wrap content-center bg-gray-400 dark:bg-gray-800 w-full h-screen text-center md:flex-row"
+      varients={containerVarients}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <motion.div className="flex items-center justify-center bg-gray-200 w-full md:w-1/2 leftSide column">
+        <div className="max-w-lg transition-all ease-in-out duration-1000 transform translate-x-0 slide">
+          <div className="example-container">
+            <motion.div className="drag-area" ref={constraintsRef} />
+            <motion.div drag dragConstraints={constraintsRef}>
+              <motion.h1
+                animate={{ marginTop: 1, opacity: 1 }}
+                initial={{ marginTop: -400, opacity: 0.2 }}
+                transition={{
+                  type: "spring",
+                  delay: 0.3,
+                  stiffness: 50,
+                  restSpeed: 0.5,
+                }}
+                className="text-2xl mb-4 transition duration-500 ease-in-out hover:text-gray-800 font-bold textShadow tracking-wide text-white dark:text-white md:text-7xl"
+              >
+                EDMOND GIHOZO
+              </motion.h1>
+            </motion.div>
+          </div>
+          <p className="mt-2 text-gray-600 mx-auto md:my-6">
+            a <strong className="text-3xl">💥Full-stack</strong> Developer &{" "}
+            <strong className="text-3xl">Designer</strong>
+          </p>
+        </div>
 
-	return (
-		<motion.div
-			className="flex flex-wrap content-center bg-gray-400 dark:bg-gray-800 w-full h-screen text-center md:flex-row"
-			varients={containerVarients}
-			initial="hidden"
-			animate="visible"
-			exit="exit"
+        <div className="inline-block align-bottom bg-gray-700 p-5 rounded md:hidden hover:bg-red-200 text-white animate-bounce">
+          <Link to="/portfolio">
+            <span>PORTFOLIO</span>
+          </Link>
+        </div>
+      </motion.div>
 
-		>
-			<motion.div className="flex items-center justify-center bg-gray-200 w-full md:w-1/2 leftSide column">
-				<div className="max-w-lg transition-all ease-in-out duration-1000 transform translate-x-0 slide">
-					<div className="example-container">
-						<motion.div className="drag-area" ref={constraintsRef} />
-						<motion.div drag dragConstraints={constraintsRef}>
-							<motion.h1
-								animate={{ marginTop: 1, opacity: 1 }}
-								initial={{ marginTop: -400, opacity: 0.2 }}
-								transition={{
-									type: "spring",
-									delay: 0.3,
-									stiffness: 50,
-									restSpeed: 0.5,
-								}}
-								className="text-2xl mb-4 transition duration-500 ease-in-out hover:text-gray-800 font-bold textShadow tracking-wide text-white dark:text-white md:text-7xl"
-							>
-								EDMOND GIHOZO
-							</motion.h1>
-						</motion.div>
-					</div>
-					<p className="mt-2 text-gray-600 mx-auto md:my-6">
-						a <strong className='text-3xl'>💥Full-stack</strong> Developer & <strong>Designer</strong>
-						<br/>
-						I specialize in <strong>Developing 😊</strong> <i>Human-entered</i>🎨 Design.
-					</p>
-				</div>
-
-				<div className="inline-block align-bottom bg-gray-700 p-5 rounded md:hidden hover:bg-red-200 text-white animate-bounce">
-					<Link to="/portfolio">
-						<span>PORTFOLIO</span>
-					</Link>
-				</div>
-			</motion.div>
-
-			<motion.div className="items-center justify-center w-full md:w-1/2 rightSide column hidden md:flex">
-				<motion.div
-					animate={{ opacity: 0.1, scale: 1, type: "spring", x: 200 }}
-					initial={{ scale: 2, x: 200 }}
-					transition={{ type: "spring", restSpeed: 0.1 }}
-					className="box1 hover:bg-red-500 box"
-					whileHover={{ scale: 2 }}
-				>
-					<FaBed size="3rem" />
-				</motion.div>
-				<motion.div
-					animate={{ opacity: 0.3, scale: 1, type: "tween", x: 40 }}
-					initial={{ scale: 2, x: 200 }}
-					transition={{ type: "tween", restSpeed: 0.2 }}
-					className="box2 hover:bg-red-600 box"
-					whileHover={{ scale: 2 }}
-				>
-					<IoFastFood size="3rem" />
-				</motion.div>
-				<motion.div
-					animate={{ opacity: 0.2, scale: 1, type: "spring", x: 100 }}
-					initial={{ scale: 2, x: 200 }}
-					transition={{ type: "spring", restSpeed: 0.3 }}
-					className="box3 hover:bg-red-700 box"
-					whileHover={{ scale: 2 }}
-				>
-					<IoCodeSlashSharp size="3rem" />
-				</motion.div>
-				<Link to={"/portfolio"}>
-					<motion.h2
-						className="md:text-4xl p-3 font-light text-left text-white titleText"
-						initial={{ fontSize: "80px", backgroundColor: "transparent" }}
-						variants={buttonVar}
-						transition={{ type: "spring", stiffness: 50, restSpeed: 0.5 }}
-						whileHover="hover"
-						animate={{
-							color: "#ffffff",
-							fontSize: "36px",
-							type: "spring",
-						}}
-					>
-						PORTFOLIO
-					</motion.h2>
-				</Link>
-			</motion.div>
-		</motion.div>
-	);
+      <motion.div className="items-center justify-center w-full md:w-1/2 rightSide column hidden md:flex">
+        <motion.div
+          animate={{ opacity: 0.1, scale: 1, type: "spring", x: 200 }}
+          initial={{ scale: 2, x: 200 }}
+          transition={{ type: "spring", restSpeed: 0.1 }}
+          className="box1 hover:bg-red-500 box"
+          whileHover={{ scale: 2 }}
+        >
+          <FaBed size="3rem" />
+        </motion.div>
+        <motion.div
+          animate={{ opacity: 0.3, scale: 1, type: "tween", x: 40 }}
+          initial={{ scale: 2, x: 200 }}
+          transition={{ type: "tween", restSpeed: 0.2 }}
+          className="box2 hover:bg-red-600 box"
+          whileHover={{ scale: 2 }}
+        >
+          <IoFastFood size="3rem" />
+        </motion.div>
+        <motion.div
+          animate={{ opacity: 0.2, scale: 1, type: "spring", x: 100 }}
+          initial={{ scale: 2, x: 200 }}
+          transition={{ type: "spring", restSpeed: 0.3 }}
+          className="box3 hover:bg-red-700 box"
+          whileHover={{ scale: 2 }}
+        >
+          <IoCodeSlashSharp size="3rem" />
+        </motion.div>
+        <Link to={"/portfolio"}>
+          <motion.h2
+            className="md:text-4xl p-3 font-light text-left text-white titleText"
+            initial={{ fontSize: "80px", backgroundColor: "transparent" }}
+            variants={buttonVar}
+            transition={{ type: "spring", stiffness: 50, restSpeed: 0.5 }}
+            whileHover="hover"
+            animate={{
+              color: "#ffffff",
+              fontSize: "36px",
+              type: "spring",
+            }}
+          >
+            PORTFOLIO
+          </motion.h2>
+        </Link>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default Home;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,84 +7,83 @@ import port from "../images/185.jpg";
 // import pimage from "../images/wp.png";
 
 const containerVarients = {
-	hidden: {
-		opacity: 0,
-		y: "-6vh",
-	},
-	visible: {
-		opacity: 1,
-		y: "0vh",
-		transition: {
-			delay: 0.7,
-			duration: 0.3,
-		},
-	},
-	exit: {
-		y: "-100vh",
-		transition: { ease: "easeInOut" },
-	},
+  hidden: {
+    opacity: 0,
+    y: "-6vh",
+  },
+  visible: {
+    opacity: 1,
+    y: "0vh",
+    transition: {
+      delay: 0.7,
+      duration: 0.3,
+    },
+  },
+  exit: {
+    y: "-100vh",
+    transition: { ease: "easeInOut" },
+  },
 };
 
 const Portfolio = () => {
-	return (
-		<motion.div
-			variants={containerVarients}
-			initial="hidden"
-			animate="visible"
-			exit="exit"
-			className="bg-red-300 h-screen"
-		>
-			<div className="dark:bg-gray-800 p-0 md:flex w-full md:h-screen text-center md:flex-row bottom-0">
-				<div className="side md:h-screen p-4 bg-red-400 top-0 w-full md:w-1/4">
-					<Link to={"/"}>
-						<motion.div
-							className="backBtn box w-14 h-14 rounded-full"
-							initial={{ backgroundColor: "#B91C1C", scale: 3 }}
-							animate={{ backgroundColor: "#991B1B", scale: 1 }}
-							transition={{ delay: 1, duration: 1.2 }}
-							whileHover={{ scale: 1.8 }}
-						>
-							<BsArrowLeft size="2em" />
-						</motion.div>
-					</Link>
+  return (
+    <motion.div
+      variants={containerVarients}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="h-screen"
+    >
+      <div className="p-0 md:flex w-full md:h-screen text-center md:flex-row bottom-0">
+        <div className="side md:h-screen bg-gray-200 p-4 top-0 w-full md:w-1/4">
+          <Link to={"/"}>
+            <div className="backBtn box w-14 h-14 ">
+              <BsArrowLeft
+                size="2em"
+                className="hover:scale-50 text-gray-800"
+              />
+            </div>
+          </Link>
 
-					<div className="info md:-mx-4">
-						<span className="text-2xl md:text-4xl font-light"> client </span>
-						<span className="text-2xl md:text-4xl font-light"> Project </span>
-						<span className="text-2xl md:text-4xl font-light"> stack </span>
-					</div>
-				</div>
+          <div className="info md:-mx-4">
+            <span className="text-2xl md:text-3xl font-bold"> Owner </span>
+            <span className="font-light"> Living Stone</span>
+            <span className="text-2xl md:text-3xl font-bold"> Type </span>
+            <span className="font-light"> Web Application</span>
+            <span className="text-2xl md:text-3xl font-bold"> Stack </span>
+            <span className="font-light"> Mongodb, Express Js, ReactJs</span>
+            <span className="text-2xl md:text-3xl font-bold"> Location </span>
+            <span className="font-light">stori.rw</span>
+          </div>
+        </div>
 
-				<div className=" md:h-screen w-full md:w-3/4 cont">
-					<div className="screen h-full md:border-red-500 border-solid border-l-2  shadow-4md">
-						<div className="titleContainer p-5 text-left">
-							<h3 className="text-2xl md:text-5xl text-red-500 align-left">
-								PROJECT TITLE
-							</h3>
-						</div>
+        <div className=" md:h-screen bg-white w-full md:w-3/4 cont">
+          <div className="screen h-full md:border-gray-500 border-solid border-l-2  shadow-4md">
+            <div className="project-title-container p-5 text-left">
+              <h3 className="text-2sm md:text-4xl text-white align-left">
+                STORI STORE
+              </h3>
+              <div className="text-white px-6 divide-white border-l-2 border-solid hover:text-red-400">
+                <Link to="/">
+                  <span className="animate-pulse">Next Project</span>
+                </Link>
+              </div>
+            </div>
 
-						<div className="project-content overflow-hidden">
-							<div className="image flex content-center align-center p-4">
-								<img
-									src={port}
-									alt="project"
-									className="md:w-full rounded-2xl shadow-2md"
-								/>
-							</div>
-						</div>
-					</div>
-
-					<motion.div
-						animate={{ opacity: 0.1, scale: 1, type: "spring", x: 200 }}
-						initial={{ scale: 2, x: 200 }}
-						transition={{ type: "spring", restSpeed: 0.1 }}
-						className="boxNext hover:bg-red-500 box"
-						whileHover={{ scale: 2 }}
-					></motion.div>
-				</div>
-			</div>
-		</motion.div>
-	);
+            <div className="project-content overflow-scroll">
+              <div className="image flex content-center align-center p-1">
+                <img
+                  src={port}
+                  alt="project"
+                  className="md:w-full rounded shadow-2md"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Portfolio;
