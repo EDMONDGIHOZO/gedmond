ac41e22ca7f03d03f7e8d9c97a8bf37cd847bb1f
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-05-23 01:53:27 +0200

    modify portfolio pictures

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import "./shared/App.css";
 import { Route, Switch, useLocation } from "react-router-dom";
 import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
-import {AnimatePresence} from 'framer-motion'
-
+import Page2 from "./components/page2.js";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   let location = useLocation();
   return (
     <div className="wrapper">
       <div className="main">
-          <AnimatePresence>
+        <AnimatePresence>
           <Switch location={location} key={location.key}>
             <Route component={Home} path="/" exact />
             <Route component={Portfolio} path="/portfolio" exact />
+            <Route component={Page2} path="/portfolio2" exact />
           </Switch>
-          </AnimatePresence>
+        </AnimatePresence>
       </div>
     </div>
   );
